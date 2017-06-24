a846b4a78e6e2649e1e8b8a82f2128db617edb99
nivarson <nivarson@gmail.com>
2017-06-24 13:25:47 -0700

    Final commit for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions calculates the inversion of square, invertible matricies.
+## It will fail with non-invertible matrices
+## It caches results, such that if it is fed a matrix it has previously calculated an inverse for,
+## it will use the cached inverse and bypass calculation
 
-## Write a short comment describing this function
+## MakeCacheMatrix generates a list of functions that write and read matrices and their inverses.
+## It does no calculation, just sets to and fetches from an outside environment (cache)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  MatInv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    MatInv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInv <- function(solve) MatInv <<- solve
+  
+  getInv <- function() MatInv
+  
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## cachesolve checks if the inverse for a matrix already exists in cache
+##If it does, it returns the cached version to avoid unnecessary calculation
+##Otherwise, it calculates it, saves that to cache, and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  MatInv <- x$getInv() #Gets the inversion from the cached environment
+  
+  if(!is.null(MatInv)) { #if the cached inversion exists, it uses the cache and exits the function with return.
+
+    message("getting cached data")
+    return(MatInv)
+    
+  }
+  
+  data <- x$get()
+  MatInv <- solve(data, ...)
+  x$setInv(MatInv)
+  
+  MatInv
 }
